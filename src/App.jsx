d3c4e9f4d20de2593744312bf4f8721c685ead4a
Dev Panchal <90882951+DevPanchal02@users.noreply.github.com>
2025-02-12 04:33:34 -0500

    increased responsiveness between components and added about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import React from "react";
 import { Bentobox } from "./components/Bentobox";
+import { About } from "./components/About";
+
 function App() {
   return (
-    <Bentobox/>
+    <div className="min-h-screen w-full flex flex-col md:flex-row bg-[#090a0c]">
+      {/* About section - 61.8% width on desktop */}
+      <section className="h-screen md:h-screen w-full md:w-[61.8%]">
+        <About className="w-full h-full" />
+      </section>
+      
+      {/* Bentobox section - 38.2% width on desktop */}
+      <section className="h-screen md:h-screen w-full md:w-[38.2%]">
+        <Bentobox />
+      </section>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const About = ({className}) => {
+    return (
+      <div className={`h-full ${className}`}>
+        hello
+      </div>
+    );
+  };
